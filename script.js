0faf237c90114a78711a24747f1c563621b9340a
Lefan Hu <lefanhu1@gmail.com>
2021-03-09 12:06:38 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+let balls = [];
+
+const FRAMES_PER_SEC = 60;
+
+class Ball {
+  constructor(radius) {
+    this.radius = radius;
+    this.posX =
+      Math.floor(Math.random() * (canvas.width - 2 * radius)) + radius;
+    this.posY =
+      Math.floor(Math.random() * (canvas.height - 2 * radius)) + radius;
+    this.colour = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    this.dx = Math.ceil(Math.random() * 5) * (Math.random() < 0.5 ? -1 : 1);
+    this.dy = Math.ceil(Math.random() * 5) * (Math.random() < 0.5 ? -1 : 1);
+    console.log(this.colour);
+  }
+}
+
+function draw() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight - 50;
+
+  for (var i = 0; i < balls.length; i++) {
+    ball = balls[i];
+    ctx.beginPath();
+    ctx.arc(ball.posX, ball.posY, ball.radius, 2 * Math.PI, false);
+    ctx.fillStyle = ball.colour;
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+function update() {
+  for (var i = 0; i < balls.length; i++) {
+    ball = balls[i];
+    ball.posX += ball.dx;
+    ball.posY += ball.dy;
+
+    radius = ball.radius;
+
+    //collision with canvas edge
+    if (ball.posX + radius > canvas.width || ball.posX - radius < 0) {
+      ball.dx *= -1;
+    }
+    if (ball.posY + radius > canvas.height || ball.posY - radius < 0) {
+      ball.dy *= -1;
+    }
+  }
+}
+
+function game() {
+  draw();
+  update();
+}
+
+function start() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight - 50;
+  numBalls = 50;
+  for (var i = 0; i < numBalls; i++) {
+    radius = Math.floor(Math.random() * (40 - 5) + 5);
+    balls.push(new Ball(radius, canvas.width, canvas.height));
+  }
+}
+
+start();
+setInterval(game, 1000 / FRAMES_PER_SEC);
