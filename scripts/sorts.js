aed0f305ec20353e1899bfe649df16bf0969d61a
Lefan Hu <lefanhu1@gmail.com>
2021-03-10 11:01:55 -0500

    added more sorts

--- scripts/sorts.js
+++ scripts/sorts.js
@@ -148,6 +148,170 @@ async function doQuickSort(items, left, right) {
   inProgress = false;
 }
 
+async function merge(left, right) {
+  var result = [],
+    lLen = left.length,
+    rLen = right.length,
+    l = 0,
+    r = 0;
+  while (l < lLen && r < rLen) {
+    if (left[l] < right[r]) {
+      result.push(left[l++]);
+    } else {
+      result.push(right[r++]);
+    }
+    data = result.concat(left.slice(l)).concat(right.slice(r));
+    await drawData();
+  }
+  //remaining part needs to be addred to the result
+  return result.concat(left.slice(l)).concat(right.slice(r));
+}
+
+async function mergeSort(arr) {
+  var len = arr.length;
+  if (len < 2) return arr;
+  var mid = Math.floor(len / 2),
+    left = arr.slice(0, mid),
+    right = arr.slice(mid);
+  //send left and right to the mergeSort to broke it down into pieces
+  //then merge those
+  return await merge(await mergeSort(left), await mergeSort(right));
+}
+
+async function doMergeSort(arr) {
+  arr = await shuffle(arr);
+  await mergeSort(arr);
+  await drawData();
+  inProgress = false;
+}
+
+async function shellSort(arr) {
+  arr = await shuffle(arr);
+  var increment = arr.length / 2;
+  while (increment > 0) {
+    for (i = increment; i < arr.length; i++) {
+      var j = i;
+      var temp = arr[i];
+
+      while (j >= increment && arr[j - increment] > temp) {
+        arr[j] = arr[j - increment];
+        j = j - increment;
+        await drawData();
+      }
+
+      arr[j] = temp;
+    }
+
+    if (increment == 2) {
+      increment = 1;
+    } else {
+      increment = parseInt((increment * 5) / 11);
+    }
+  }
+
+  await drawData();
+  inProgress = false;
+  return arr;
+}
+
+async function radixBucketSort(arr) {
+  arr = await shuffle(arr);
+  var idx1, idx2, idx3, len1, len2, radix, radixKey;
+  var radices = {},
+    buckets = {},
+    num,
+    curr;
+  var currLen, radixStr, currBucket;
+
+  len1 = arr.length;
+  len2 = 10; // radix sort uses ten buckets
+
+  // find the relevant radices to process for efficiency
+  for (idx1 = 0; idx1 < len1; idx1++) {
+    radices[arr[idx1].toString().length] = 0;
+  }
+
+  // loop for each radix. For each radix we put all the items
+  // in buckets, and then pull them out of the buckets.
+  for (radix in radices) {
+    // put each array item in a bucket based on its radix value
+    len1 = arr.length;
+    for (idx1 = 0; idx1 < len1; idx1++) {
+      curr = arr[idx1];
+      // item length is used to find its current radix value
+      currLen = curr.toString().length;
+      // only put the item in a radix bucket if the item
+      // key is as long as the radix
+      if (currLen >= radix) {
+        // radix starts from beginning of key, so need to
+        // adjust to get redix values from start of stringified key
+        radixKey = curr.toString()[currLen - radix];
+        // create the bucket if it does not already exist
+        if (!buckets.hasOwnProperty(radixKey)) {
+          buckets[radixKey] = [];
+        }
+        // put the array value in the bucket
+        buckets[radixKey].push(curr);
+      } else {
+        if (!buckets.hasOwnProperty("0")) {
+          buckets["0"] = [];
+        }
+        buckets["0"].push(curr);
+      }
+    }
+    // for current radix, items are in buckets, now put them
+    // back in the array based on their buckets
+    // this index moves us through the array as we insert items
+    idx1 = 0;
+    // go through all the buckets
+    for (idx2 = 0; idx2 < len2; idx2++) {
+      // only process buckets with items
+      if (buckets[idx2] != null) {
+        currBucket = buckets[idx2];
+        // insert all bucket items into array
+        len1 = currBucket.length;
+        for (idx3 = 0; idx3 < len1; idx3++) {
+          arr[idx1++] = currBucket[idx3];
+          await drawData();
+        }
+      }
+    }
+    buckets = {};
+  }
+  inProgress = false;
+}
+
+async function cocktailSort(nums) {
+  nums = await shuffle(nums);
+  let is_Sorted = true;
+  while (is_Sorted) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        is_Sorted = true;
+      }
+    }
+    await drawData();
+
+    if (!is_Sorted) break;
+
+    is_Sorted = false;
+
+    for (let j = nums.length - 1; j > 0; j--) {
+      if (nums[j - 1] > nums[j]) {
+        let temp = nums[j];
+        nums[j] = nums[j - 1];
+        nums[j - 1] = temp;
+        is_Sorted = true;
+      }
+    }
+    await drawData();
+  }
+  inProgress = false;
+}
+
 //currently not utilized
 function drawComparisons() {
   ctx.font = "128px Arial";
@@ -164,7 +328,7 @@ async function drawData() {
     ctx.fillRect(
       i * block.width,
       canvas.height - data[i] * block.height,
-      block.width,
+      block.width + 1,
       data[i] * block.height
     );
   }
@@ -200,6 +364,18 @@ document.addEventListener("keydown", function (e) {
         break;
       case "quick":
         doQuickSort(data, 0, data.length - 1);
+        break;
+      case "merge":
+        doMergeSort(data);
+        break;
+      case "shell":
+        shellSort(data);
+        break;
+      case "radix":
+        radixBucketSort(data);
+        break;
+      case "cocktail":
+        cocktailSort(data);
         break;
     }
     drawInstructions();
